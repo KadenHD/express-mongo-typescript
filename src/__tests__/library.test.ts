2c5cc3dc85ac6419b9873c4da7a3f310ac3f6092
KadenHD <pierraored@gmail.com>
2022-10-02 20:05:38 +0200

    Update library.test.ts

---
+++ src/__tests__/library.test.ts
@@ -0,0 +1,22 @@
+import Encrypter from '../library/Encrypter';
+import Logging from '../library/Logging';
+
+describe('src/library/Encrypter.ts', () => {
+    describe('encrypt', () => {
+        it('', () => {});
+    });
+
+    describe('decrypt', () => {
+        it('', () => {});
+    });
+});
+
+describe('src/library/Logging.ts', () => {
+    describe('', () => {
+        it('', () => {});
+    });
+
+    describe('', () => {
+        it('', () => {});
+    });
+});
